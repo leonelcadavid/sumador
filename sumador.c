5d5aa5db1d4b798bea58f4d7e7e003b87dbb3d0e
Victoria Duarte <victoria.duarte@alu.inspt.utn.edu.ar>
2024-04-17 20:55:11 -0300

    Sumador v1.3

--- sumador.c
+++ sumador.c
@@ -3,12 +3,14 @@
 #define s scanf
 
 int main (void){
-    int num1,num2,suma;   
+    int num1,num2,num3,suma;   
     p("ingrese primer valor: ");
     s("%d",num1);
     p("ingrese segundo valor: ");
     s("%d",num2);
-    suma=num1+num2;
-    p("%d+%d=%d",num1,num2,suma);
+    p("ingrese tercer valor: ");
+    s("%d",num3);
+    suma=num1+num2+num3;
+    p("%d+%d+%d=%d",num1,num2,num3,suma);
 
 return 0;}
